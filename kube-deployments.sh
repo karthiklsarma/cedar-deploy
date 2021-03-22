dc097ba292d2550b0a53c0138d27e3e69f503aa1
Karthik Sarma <karthiklsarma@gmail.com>
2021-03-21 17:27:50 -0700

    updated readme instruction

--- kube-deployments.sh
+++ kube-deployments.sh
@@ -5,5 +5,11 @@
 # kubelogin
 
 az aks get-credentials --resource-group cedar-rg --name cedar-ks
+# If config already exists or if you are running this script the second time, you can choose to keep the config
+#A different object named cedar-ks already exists in your kubeconfig file.
+#Overwrite? (y/n): n
+#A different object named clusterUser_cedar-rg_cedar-ks already exists in your kubeconfig file.
+#Overwrite? (y/n): n
+
 kubectl apply -f ./deployments/cedar-engine.yaml
 kubectl apply -f ./services/cedar-engine.yaml
